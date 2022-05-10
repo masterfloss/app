22f033edb934fe72479257d18bf58c79bce2319a
masterfloss <carlos.costa@acm.org>
2022-05-10 01:48:11 +0100

    update

--- myapp.py
+++ myapp.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 a= {"name":[1,2,5,2,6],"address":[3,5,7,3,1]}
 b=pd.DataFrame(a)
 b.plot("name","address")
 st.write(b)
+st.line_chart(b)
