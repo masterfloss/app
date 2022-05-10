a35a26d1cbad78086a82e76e388a0a852d1517bb
masterfloss <carlos.costa@acm.org>
2022-05-11 00:38:57 +0100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 a= {"serie 1":[1,2,5,2,6],"fora de serie":[3,5,7,3,1]}
 b=pd.DataFrame(a)
-st.write("<a href="http://isegtech.org">sata</a>")
+st.write('<a href="http://isegtech.org">sata</a>')
 st.write(b)
 st.line_chart(b)
