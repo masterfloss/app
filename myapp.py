b3d4f545db008d94c302fbf73a4c0aba14a2fc15
masterfloss <carlos.costa@acm.org>
2022-05-10 01:50:54 +0100

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-a= {"name":[1,2,5,2,6],"address":[3,5,7,3,1]}
+a= {"serie 1":[1,2,5,2,6],"fora de serie":[3,5,7,3,1]}
 b=pd.DataFrame(a)
 
 st.write(b)
